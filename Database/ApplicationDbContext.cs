593d8c516ba1ca98fcc272ee3413f562cdbcb9e8
thiagobardella <thiago.bardella@gmail.com>
2020-12-27 20:53:00 -0300

    First

---
+++ Database/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+
+namespace Auth_Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        //Asp.Net Core Identity Model Config
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+        }
+    }
+}
